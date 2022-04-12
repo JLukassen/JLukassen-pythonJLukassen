4b07b5731e414d1e5b161c8165624c31f1bb2261
JLukassen <jonlukassen@gmail.com>
2022-04-12 12:20:23 -0500

    first commit personal resume revised as python web app

--- django_jlwebsite/settings.py
+++ django_jlwebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gnxc-vec#_gp&k257vov^nnf!%^yzh+&qad=^9*xl3nay3)c=-'
 
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
